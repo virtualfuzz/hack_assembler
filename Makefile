a05325ab51aab86242d2f802c1c3a265b0a97cb5
Jayden <c.luong997@proton.me>
2024-11-10 16:18:56 +0100

    feat: Change how options are handled, update compiler, etc...
    Change how options work, use optarg now, update_labels is now mostly compiling instead of just updating labels, potentially other changes that are smaller then those bigger changes

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 CC      =	cc
+CFLAGS	=	-Werror -Wall -Wextra
 CFLAGS  =	-Werror -Wall -Wextra -O3 -pipe -D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS \
 			-fstack-protector-strong -fstack-clash-protection -fcf-protection \
 			-fpie -fpic
@@ -7,5 +8,8 @@ LDFLAGS	=	-Wl,-z,now -Wl,-z,relro
 release: src/hack_assembler.c
 	${CC} ${CFLAGS} ${LDFLAGS} src/hack_assembler.c -o builds/hack_assembler
 
+test: release src/hack_assembler.c test/asm/Add.asm
+	./builds/hack_assembler -i ./test/asm/Add.asm -o ./test/to_compare/Add.hack
+
 clean:
-	rm hack_assembler
+	rm builds/hack_assembler
--- src/hack_assembler.c
+++ src/hack_assembler.c
@@ -1,24 +1,120 @@
+#include <ctype.h>
+#include <getopt.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void usage(char *program);
 void update_labels(FILE *fp);
-void error(char *type, char* message, ...);
+void error(char *type, char *message, ...);
+enum instruction { NONE, LABEL, SLASH_ONE, SLASH_TWO, A_INSTRUCT };
 
 int main(int argc, char **argv) {
-  // Check if there is only one argument
-  if (argc != 2) {
+  // Options
+  bool force = false;
+  char *source_filename = NULL;
+  char *output_filename = NULL;
+
+  // Options that our compiler takes
+  struct option long_options[] = {{"force", no_argument, 0, 'f'},
+                                  {"input", required_argument, 0, 'i'},
+                                  {"output", required_argument, 0, 'o'},
+                                  {0, 0, 0, 0}};
+
+  // Get options
+  int opt;
+  while ((opt = getopt_long(argc, argv, "fi:o:", long_options, NULL)) != -1) {
+    switch (opt) {
+    case 'f':
+      force = 1;
+      break;
+    case 'i':
+      source_filename = optarg;
+      break;
+    case 'o':
+      output_filename = optarg;
+      break;
+    case '?':
+      usage(argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Source filename is required
+  if (source_filename == NULL) {
     usage(argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  // Try to open file
+  // Try to open source file
   FILE *assembly;
-  assembly = fopen(argv[1], "r");
+  assembly = fopen(source_filename, "r");
   if (assembly == NULL) {
-    error("", "\"%s\" doesn't exist\n", argv[1]);
+    error("FILE", "\"%s\" doesn't exist\n", source_filename);
+  }
+
+  // Name of the hack save file
+  char *hack_save_file;
+
+  // If we gave the name of the hack file set it as that
+  // Else create the file name using the filename of source file
+  if (output_filename) {
+    hack_save_file = output_filename;
+  } else {
+    // Copy into a new string source filename
+    // Reason: We used to set hack_save_file to source but it will modify source
+    // too
+    size_t length = strlen(source_filename);
+    size_t allocation_size = length + 2;
+
+    hack_save_file = (char *)malloc(allocation_size); // FIXME: Free me
+    if (hack_save_file == NULL) {
+      error("MEMORY ",
+            "Failed to allocate %zu bytes for hack_save_file string\n",
+            allocation_size);
+    }
+
+    hack_save_file = strcpy(hack_save_file, source_filename);
+
+    // Remove .asm from the end of the file if it exists
+    if (4 < length) {
+
+      // Get last 4 characters of hack_save_file (first arg, asm filename)
+      char to_compare[5];
+      strncpy(to_compare, hack_save_file + length - 4, 4);
+
+      if (strcmp(to_compare, ".asm") == 0) {
+        // Remove the last 4 characters (.asm)
+        hack_save_file[length - 4] = '\0';
+      }
+    }
+
+    // Add .hack to the filename
+    strcat(hack_save_file, ".hack");
+  }
+
+  // Check if file exists to prevent overwriting
+  // TODO: but what if the file is auto created?
+  if (access(hack_save_file, F_OK) != -1) {
+    if (force == true) {
+      printf("empty file or some shit like that\n");
+      printf("we didnt finish this shit\n");
+      exit(EXIT_FAILURE);
+    } else {
+      error("FILE ", "%s already exists, use the --force flag to overwrite file\n", hack_save_file);
+    }
+  }
+
+  // Create compiled code file
+  FILE *write_to;
+  write_to = fopen(hack_save_file, "w");
+  if (write_to == NULL) {
+    error("FILE ",
+          "Failed to create file %s maybe a directory doesn't exist...",
+          hack_save_file);
   }
 
   // First pass: Only update the symbol table
@@ -44,7 +140,8 @@ void error(char *type, char *message, ...) {
 
 // Print usage
 void usage(char *program) {
-  printf("USAGE: %s foo.asm\n", program);
+  printf("USAGE: FIXME Usage: %s [-f] [-i input] [-o output]\n", program);
+  printf("USAGE: %s foo.asm foo.hack\n", program);
   printf("Creates a file named foo.hack that can be "
          "executed on the Hack computer.\n");
 }
@@ -59,22 +156,34 @@ void update_labels(FILE *fp) {
   // Loop though each line
   while ((read = getline(&line, &len, fp)) != -1) {
 
-    // Loop though each character in line
     size_t line_len = strlen(line);
-    int new_label = 0;
-    int slash = 0;
-    char label_name[line_len];
-    strcpy(label_name, "");
+    enum instruction current_instruct = NONE;
+    char value[line_len];
+    strcpy(value, "");
 
+    // Loop though each character in line
     for (size_t i = 0; i < line_len; i++) {
-      if (new_label == 1 && line[i] != ')') {
-        strncat(label_name, &line[i], 1);
+      // TODO: Rework on that
+      // Copy character if we are currently in a label
+      // set instruction type to like label or a instruct
+      // do something depending on that
+      if (current_instruct == LABEL && line[i] != ')') {
+        strncat(value, &line[i], 1);
       }
 
+      // TODO: Document that
       switch (line[i]) {
+      case '@':
+        if (current_instruct == NONE) {
+          current_instruct = A_INSTRUCT;
+          printf("a instruction\n");
+        } else {
+          error("SYNTAX ", "Unexpected @ at line %zu\n", current_line);
+        }
+        break;
       case '(':
-        if (new_label != 1 && strlen(label_name) == 0) {
-          new_label = 1;
+        if (current_instruct == NONE && strlen(value) == 0) {
+          current_instruct = LABEL;
         } else {
           fclose(fp);
           free(line);
@@ -82,8 +191,8 @@ void update_labels(FILE *fp) {
         }
         break;
       case ')':
-        if (new_label != 0) {
-          new_label = 0;
+        if (current_instruct != NONE) {
+          current_instruct = NONE;
         } else {
           fclose(fp);
           free(line);
@@ -91,19 +200,42 @@ void update_labels(FILE *fp) {
         }
         break;
       case '/':
-        slash++;
+        switch (current_instruct) {
+        case SLASH_ONE:
+          current_instruct = SLASH_TWO;
+          break;
+        case NONE:
+          current_instruct = SLASH_ONE;
+          break;
+        default:
+          error("SYNTAX ", "WAHT THE FUKC\n", current_line);
+        }
         break;
+      default:
+        // TODO: case thing
+        // Ignore whitespace
+        if (isspace(line[i])) {
+        } else if (current_instruct == A_INSTRUCT && isdigit(line[i])) {
+          strncat(value, &line[i], 1);
+        } else {
+          error("SYNTAX ", "Unexepected character (%c) on line %zu\n", line[i],
+                current_line);
+        }
       }
 
-      if (slash == 2) {
+      if (current_instruct == SLASH_TWO) {
         break;
       }
     }
 
-    if (strlen(label_name)) {
-      printf("%s\n", label_name);
+    // TODO: In here compile the instruction
+
+    if (strlen(value)) {
+      // FIXME: We also need to know on what address we are
+      // How about we consider everything that isn't a // or space as a command
+      printf("%s\n", value);
     }
-    
+
     current_line++;
   }
 
